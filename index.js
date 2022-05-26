4c75360093109d89262a773135ce4d3dd140b819
codayomide <smokayomide@gmail.com>
2022-05-26 16:16:08 +0100

    commit on 'master'

---
+++ index.js
@@ -0,0 +1,21 @@
+// let id = (id) => document.getElementById(id);
+// let classes = (classes) => document.getElementsByClassName(classes);
+
+
+// const dropdownBtns = classes("dropdown-btns"),
+//     dropdownBtns1 = id("dropdown-btns1"),
+//     dropdownBtns2 = id("dropdown-btns2");
+
+// let closed = true 
+
+// if (closed) {
+//     console.log("dropdown is closed")
+//     console.log(closed)
+// } 
+
+// dropdownBtns1.addEventListener("click", () => {
+//     closed = false
+//     console.log("dropdown is open")
+//     console.log(closed)
+//     dropdownBtns1.innerHTML = `Features <img src="images/icon-arrow-up.svg">`
+// })
